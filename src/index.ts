709ac13e8e843bb49eb329f04515254931d0da7f
eunjukim-tech <sb02348@hanmail.net>
2022-04-11 23:55:13 +0900

    Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,29 @@
 //js에서 interface 사용하고 싶으면 class 활용
-class Human {
-  public name: string;
-  private age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const chaei = new Human("Chaei", 28, "female");
+const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
+let blockchain: [Block] = [genesisBlock];
 
-const sayHi = (person: Human):string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
-
-console.log(sayHi(chaei));
+console.log(blockchain);
 
 export {};
